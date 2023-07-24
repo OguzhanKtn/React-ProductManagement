beda608d9a4d80cd9670d30f6abb88a8db7fa330
Oğuzhan <81297977+OguzhanKtn@users.noreply.github.com>
2023-07-24 22:07:02 +0300

    BasketPageUpdated

--- src/pages/Basket.tsx
+++ src/pages/Basket.tsx
@@ -3,54 +3,70 @@ import { Product } from "../model/DummyProducts";
 import { singleProduct } from "../service";
 
 function Basket() {
+  const [productStrings, setProductStrings] = useState<string[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
-  const [proArr, setProArr] = useState<string[]>([])
-  const [prd, setPrd] = useState<Product[]>([])
+  useEffect(() => {
+    const basket = localStorage.getItem("basket");
+    if (basket) {
+      const parsedBasket: string[] = JSON.parse(basket) as string[];
+      setProductStrings(parsedBasket);
+    }
+  }, []); 
 
-useEffect(() => {
+  useEffect(() => {
+    const currentProducts = Object.assign([], products);
+    const asyncCallback = async () => {
+      for (let productStr of productStrings) {
+        try {
+          const product = await singleProduct(productStr);
+          currentProducts.push(product.data);
+        } catch (err: any) {
+          console.error(err.message);
+        }
+      }
 
-  const stArr = localStorage.getItem("basket")
-  if(stArr){
-   const arr:string[] = JSON.parse(stArr) as string[]
-   setProArr(arr)
-  }
-  const arr2 = Object.assign([],prd)
- { proArr.map((item,index)=>
-  singleProduct(item).then((res)=>{
-    arr2.push(res.data)
-  }).catch(err =>{
-    console.log(err.message)
-  })
-  )}
- setPrd(arr2)
-}, [])
+      setProducts(currentProducts);
+    };
+
+    asyncCallback();
+  }, []);
+
+  const fncDelete = (index: number) => {
+    const newArr = Object.assign([], productStrings) 
+    newArr.splice(index, 1)
+    setProductStrings(newArr)
+    localStorage.setItem("basket", JSON.stringify(newArr))
+}
 
   return (
     <>
       <table className="table">
         <thead>
           <tr>
-          <th scope="col">Image</th>
+            <th scope="col">Image</th>
             <th scope="col">Product Name</th>
             <th scope="col">Product Price</th>
             <th scope="col">Amount</th>
+            <th scope="col">Delete</th>
           </tr>
         </thead>
-        <tbody>               
-           { 
-           prd.map((item,index) =>(   
+        <tbody>
+          {products.map((item, index) => (
             <tr key={index}>
-                <td> 
-                  <div className="col-2">
-                  <img src={item.images[0]} className='img-thumbnail' />
+              <td>
+                <div className="col-2">
+                  <img src={item.images[0]} className="img-thumbnail" />
                 </div>
-                </td>
-                <td>{item.brand}</td>
-                <td>{item.price}$</td>
-                <td>{item.stock}</td>
-              </tr> 
-            ))   
-           }            
+              </td>
+              <td>{item.brand}</td>
+              <td>{item.price}$</td>
+              <td>{item.stock}</td> 
+              <td>
+              <button className="btn btn-sm btn-danger" onClick={ () => fncDelete(index)} role='button'>Delete</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
